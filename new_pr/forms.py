38b78d83f52469ad1f059ff943c44a5d7159c148
akash-1120 <aakashyadav7226@gmail.com>
2026-04-13 18:22:10 +0530

    the final

--- new_pr/forms.py
+++ new_pr/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField ,PasswordField , SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 class RegistrationForm(FlaskForm):
-    name = StringField("full name", validators = [DataRequired()])
-    email = StringField("Email" , validators= [DataRequired(), Email()])
-    password = PasswordField("Password" , validators= [DataRequired(), Length(min= 6)])
-    Submit = SubmitField("Register")
+    name = StringField("full name", validators = [DataRequired(message = "Enter your full name ")])
+    email = StringField("Email" , validators= [DataRequired(message = "enter your office email"), Email(message= "only valid email is allowed")])
+    password = PasswordField("Password" , validators= [DataRequired(message= "password must be 6 charchter"), Length(min= 6, message= "minmum 6 charchter")])
+    submit = SubmitField("Register")
