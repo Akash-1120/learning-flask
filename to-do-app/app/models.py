33faa1cae6b54c992cfdbf4cf41ee9989f404e77
akash-1120 <aakashyadav7226@gmail.com>
2026-04-14 18:47:09 +0530

    to-do-app using Flask

---
+++ to-do-app/app/models.py
@@ -0,0 +1,5 @@
+from app import db
+class Task(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.string(100), nullable = False)
+    status = db.Column(db.String(20),deafult = "Pending")
